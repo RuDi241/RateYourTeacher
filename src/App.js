244067cf99b1fa171a7d999e0e70747d4ce5d7e3
maraytakarim <maraytakarim@gmail.com>
2020-07-03 20:58:56 +0300

    Initial commit + react-router setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import LoginPage from "./components/LoginPage";
+import SignUpPage from "./components/SignUpPage";
+import CommentsPage from "./components/CommentsPage";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route path="/" component={HomePage} />
+      <Route path="/login" component={LoginPage} />
+      <Route path="/signup" component={SignUpPage} />
+      <Route path="/comments" component={CommentsPage} />
     </div>
   );
 }
---
+++ src/components/CommentsPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CommentsPage() {
+  return <div></div>;
+}
+
+export default CommentsPage;
