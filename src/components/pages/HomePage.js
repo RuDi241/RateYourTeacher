933b2ac70f4f422238797a72583a377e7265a56d
maraytakarim <maraytakarim@gmail.com>
2020-07-15 01:38:19 +0300

    removed unused import

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Welcome from "../home/homePageWelcome/Welcome";
 class HomePage extends React.Component {
   constructor(props) {
